1f594a5fbb55448bd1a09d439f2e65e917b98931
Anna Rylander <annarylander@MacBook-Air-som-tillhor-Anna.local>
2022-09-19 16:18:20 +0200

    lektion 5

---
+++ lektion5.py
@@ -0,0 +1,37 @@
+#1
+course = "Programmering 1"
+points = 100
+
+print(f"Vi läser {course} som är {points} poäng" )
+
+#2
+message = "prata högre"
+message.upper()
+
+#3
+message2 = "SKRIK INTE TACK"
+message2.lower()
+
+#4
+message3 = "Vi lär oss programmera"
+len(message)
+
+#5
+text = "Jag gillar java"
+text.replace("java", "python")
+
+
+#6
+text2 = "Jag gillar python"
+text.split()
+
+#7
+name = input("Skriv in ditt namn: ")
+first = name[0]
+print(f"Första bokstaven är {first}")
+
+#8
+txt = "The quick brown fox jumps over the lazy dog"
+
+print(txt.find("dog"))
+print("dog" in txt)
